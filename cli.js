2b6a9303c241f69394712d858e8733a6a1973d89
mskradde <skradde.marco@gmail.com>
2020-08-26 15:27:50 +0200

    Encrypt masterpassword

--- cli.js
+++ cli.js
@@ -5,18 +5,27 @@ const {
   CHOICE_GET,
   CHOICE_SET,
 } = require("./lib/questions");
-const { readPassword, writePassword } = require("./lib/passwords");
+const {
+  readPassword,
+  writePassword,
+  readMasterpassword,
+} = require("./lib/passwords");
 
 async function main() {
   const { masterPassword, action } = await askStartQuestions();
+  const originalMasterPassword = await readMasterpassword();
 
-  if (masterPassword === "123") {
-    console.log("Master Password is correct!");
-    if (action === CHOICE_GET) {
-      console.log("Now Get a password");
-      const { key } = await askGetPasswordQuestions();
+  if (masterPassword !== originalMasterPassword) {
+    const {newMasterPas}
+    console.log("Master Password is incorrect!");
+    return;
+  }
+  console.log("Master Password is correct!");
+  if (action === CHOICE_GET) {
+    console.log("Now Get a password");
+         const { key } = await askGetPasswordQuestions();
       try {
-        const password = await readPassword(key);
+        const password = await readPassword(key, masterPassword);
         console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
@@ -25,12 +34,10 @@ async function main() {
     } else if (action === CHOICE_SET) {
       console.log("Now Set a password");
       const { key, password } = await askSetPasswordQuestions();
-      await writePassword(key, password);
+      await writePassword(key, password, masterPassword);
       console.log(`New Password set`);
     }
-  } else {
-    console.log("Master Password is incorrect!");
-  }
+
 }
 
 main();
