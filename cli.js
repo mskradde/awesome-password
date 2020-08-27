e48b246d793a23709029a34ef057726d8bf47dd5
mskradde <skradde.marco@gmail.com>
2020-08-27 08:05:26 +0200

    Install and add mongodb

--- cli.js
+++ cli.js
@@ -4,40 +4,59 @@ const {
   askSetPasswordQuestions,
   CHOICE_GET,
   CHOICE_SET,
+  askForNewMasterPassword,
 } = require("./lib/questions");
 const {
   readPassword,
   writePassword,
-  readMasterpassword,
+  readMasterPassword,
+  writeMasterPassword,
 } = require("./lib/passwords");
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://mskradde:8j6yNdVqwQxuZLs@development.jgwho.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
 
 async function main() {
-  const { masterPassword, action } = await askStartQuestions();
-  const originalMasterPassword = await readMasterpassword();
+  try {
+    await client.connect();
+    const database = client.db("awesome-passwords");
+    const collection = database.collection("passwords");
 
-  if (masterPassword !== originalMasterPassword) {
-    const {newMasterPas}
-    console.log("Master Password is incorrect!");
-    return;
-  }
-  console.log("Master Password is correct!");
-  if (action === CHOICE_GET) {
-    console.log("Now Get a password");
-         const { key } = await askGetPasswordQuestions();
+    const { masterPassword, action } = await askStartQuestions();
+    const originalMasterPassword = await readMasterPassword();
+
+    if (!originalMasterPassword) {
+      const { newMasterPassword } = await askForNewMasterPassword();
+      await writeMasterPassword(newMasterPassword);
+      return;
+    }
+    if (masterPassword !== originalMasterPassword) {
+      console.log("Master Password is incorrect!");
+      return;
+    }
+
+    if (action === CHOICE_GET) {
+      console.log("Now Get a password");
+      const { key } = await askGetPasswordQuestions();
       try {
         const password = await readPassword(key, masterPassword);
         console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
-        // What to do now?
       }
     } else if (action === CHOICE_SET) {
       console.log("Now Set a password");
       const { key, password } = await askSetPasswordQuestions();
       await writePassword(key, password, masterPassword);
       console.log(`New Password set`);
+    } finally {
+      await client.close();
     }
-
+   
+  }
 }
 
 main();
